9f228b442f1d89bae65381ac8ff34a8ff94846ba
Derek Chiu <dchiu@stat.ubc.ca>
2014-11-18 00:30:30 -0800

    Version 0.0.0.9001

--- tests/testthat/test_me.R
+++ tests/testthat/test_me.R
@@ -1,3 +1,24 @@
 test_that("case is ignored", {
 	expect_equal(gday("canucks"), gday("CANUCKS"))
 })
+
+test_that("always returns logical", {
+	expect_is(gday("canucks"), "logical")
+})
+
+test_that("asking for the city works just as well", {
+	expect_equal(gday("canucks"), gday("Vancouver"))
+})
+
+test_that("Seattle does not have a NHL team", {
+	expect_false(gday(team="Seattle"))
+})
+
+test_that("Vancouver Canucks had a game against Nashville Predators on 2014-11-02", {
+	expect_true(gday(team = "canucks",   date = "2014-11-02"))
+	expect_true(gday(team = "predators", date = "2014-11-02"))
+})
+
+test_that("New York Rangers did not have a game on 2014-11-12", {
+	expect_false(gday(team = "rangers",   date = "2014-11-12"))
+})
